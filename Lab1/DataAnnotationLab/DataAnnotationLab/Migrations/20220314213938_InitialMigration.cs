f39651e88c8e5dcc531df59b373a5ee1e462b904
musab <yaqoobmusab03@gmail.com>
2022-03-14 18:23:01 -0500

    SD-340 Lab1 Submission

---
+++ Lab1/DataAnnotationLab/DataAnnotationLab/Migrations/20220314213938_InitialMigration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAnnotationLab.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    Section = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+        }
+    }
+}
---
+++ Lab1/DataAnnotationLab/DataAnnotationLab/Models/Client.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotationLab.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        [StringLength(25, MinimumLength = 3, ErrorMessage= "A minimum length of three characters and a max of 25.")]
+        public string FirstName { get; set; }
+        [StringLength(25, MinimumLength = 3, ErrorMessage = "A minimum length of three characters and a max of 25.")]
+        [Required]
+        public string LastName { get; set; }
+        [DataType(DataType.PhoneNumber, ErrorMessage ="Not a phone number try again")]
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public Client()
+        {
+
+        }
+        public Client(string firstName, string lastName, string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+        }
+
+    }
+}
+
+/*
+An ID (int will be sufficient here)
+
+A FirstName and LastName. The last name is required, and both have a minimum length of three characters and a max of 25.
+
+PhoneNumber, a string which must represent a phone number and have an appropriate validation message. 
+It is also required in order to register a Client. Search for a way to validate phone numbers in C#.
+ */
