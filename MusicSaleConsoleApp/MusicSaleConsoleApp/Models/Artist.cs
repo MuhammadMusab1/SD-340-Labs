58c58a0e994b4eb675b29f31ded8e66c5b4c2f41
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-03-19 22:06:14 -0500

    add Artist and Customer class (children of the User abstract class)

---
+++ MusicSaleConsoleApp/MusicSaleConsoleApp/Models/Artist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicSaleConsoleApp.Models
+{
+    internal class Artist : User
+    {
+        public ICollection<Song> Songs { get; set; }
+        public Artist()
+        {
+            Songs = new HashSet<Song>();
+        }
+    }
+}
--- MusicSaleConsoleApp/MusicSaleConsoleApp/Models/Customer.cs
+++ MusicSaleConsoleApp/MusicSaleConsoleApp/Models/Customer.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace MusicSaleConsoleApp.Models
 {
-    internal class Song
+    internal class Customer : User
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int ArtistId { get; set; }
-        public Artist Artist { get; set; }
         public ICollection<CustomerSong> CustomerSongs { get; set; }
+        public Customer()
+        {
+            CustomerSongs = new HashSet<CustomerSong>();
+        }
     }
 }
