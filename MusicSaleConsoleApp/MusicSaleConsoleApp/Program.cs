3d49d666c00556aa82d0e143adae0bb4e84ebac8
musab <yaqoobmusab03@gmail.com>
2022-03-19 21:19:10 -0500

    initial console app with instructions and a Models folder

---
+++ MusicSaleConsoleApp/MusicSaleConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+
+/*
+PART 1
+
+Create a new directory, Models. Within it, 
+create model equivalents to your Itunes project beginning with Song (int Id, string Title).
+
+Create an abstract class, 
+User (int Id, string Name, List<Song> Collection, string email). 
+
+Create for User the method GenerateEmail, 
+which appends their ID to their Name to generate an email for them.
+
+Create the classes Artist and Customer to implement User. 
+What do you need to add to these classes to have them inherit properly from User?
+
+Add the ArtistId, Artist, and Customers properties to Song.
+
+Have the Artist override the GenerateEmail method to prepend 
+an A to the email generated in the base class.
+ */
